dcac5fb4c1b0bccbf7cc114c977be923dfcaa487
Ganesh kumar S <Ganesh kumar S>
2017-10-26 01:04:46 +0530

    Add project files.

---
+++ Hive2.0/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hive2._0.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(Hive2._0.Models.User userModel)
+        {
+            using (Models.Ingenious_BeeHiveEntities db =new Models.Ingenious_BeeHiveEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName &&
+                                                       x.Password == userModel.Password).FirstOrDefault(); //Validate Username & password
+
+                if(userDetails==null)                   //Validate IF Username & password incorrect
+                {
+                    userModel.LoginErrorMessage = "Wrong UserName and Password";
+                    return View("Index",userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserId;
+                    Session["userName"] = userDetails.UserName;
+
+                    return RedirectToAction("Index","Home");
+                }
+
+            }
+               
+        }
+
+        public ActionResult LogOut()
+        {
+            int userID = (int)Session["userID"];        //To show Active User
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+        }
+    }
+}
